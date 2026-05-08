e95a83108fe0029f76093fb04d3fafaf262f6b57
Muchamad Fatchur Rachman <fatchurrachman001@gmail.com>
2026-05-08 09:24:36 +0700

    membuat koneksi ke database

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$databaseHost = 'localhost';
+
+$databaseName = 'ftrans';
+
+$databaseUsername = 'root';
+
+$databasePassword = '';
+
+
+
+// Make sure the variable name is $mysqli
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+?>
